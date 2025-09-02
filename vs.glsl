e9a82df21d5554fbf313d2e7381b5e72c403ef9e
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2025-09-01 22:47:54 -0600

    zooming with scroll wheel

--- vs.glsl
+++ vs.glsl
@@ -11,7 +11,15 @@ void main()
 	wPos = vPos;
 	float aspect = viewport.x/viewport.y;
 	wPos.x += sign(wPos.x) * (aspect / 2.0);
-	wPos /= viewport.z; //handle zooming
+	//handle zooming
+	if(viewport.z > 1.0)
+	{
+		wPos /= viewport.z;
+	}
+	if(viewport.z < -1.0)
+	{
+		wPos *= (-1.0*viewport.z);
+	}
 	wPos += offset; //handle panning
 	gl_Position = vec4(vPos, 0.0, 1.0);
 }
